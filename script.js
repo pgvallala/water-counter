71f5d746112002b3646fd1d5fe749de5654d039a
pranav157 <pranavgupta.v@gmail.com>
2021-09-22 02:05:57 +0200

    feat bigCup update function

---
+++ script.js
@@ -0,0 +1,30 @@
+const smallCups = document.querySelectorAll(".cup-small");
+const liters = document.getElementById("liters");
+const percentage = document.getElementById("percentage");
+const remained = document.getElementById("remained");
+
+updateBigCup();
+
+smallCups.forEach((cup, idx) => {
+  cup.addEventListener("click", () => hilghlightCups(idx));
+});
+
+function hilghlightCups(idx) {
+  if (idx === 11 && smallCups[idx].classList.contains("full")) idx--;
+  else if (
+    smallCups[idx].classList.contains("full") &&
+    !smallCups[idx].nextElementSibling.classList.contains("full")
+  ) {
+    idx--;
+  }
+
+  smallCups.forEach((cup, idx2) => {
+    if (idx2 <= idx) {
+      cup.classList.add("full");
+    } else {
+      cup.classList.remove("full");
+    }
+  });
+
+  updateBigCup();
+}
