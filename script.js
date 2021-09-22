c784b69982cc2f2b5017d63000754308bb1e8a99
pranav157 <pranavgupta.v@gmail.com>
2021-09-22 02:29:18 +0200

    feat: bigCup update function

--- script.js
+++ script.js
@@ -28,3 +28,25 @@ function hilghlightCups(idx) {
 
   updateBigCup();
 }
+
+function updateBigCup() {
+  const fullCups = document.querySelectorAll(".cup-small.full").length;
+  const totalCups = smallCups.length;
+
+  if (fullCups === 0) {
+    percentage.style.visibility = "hidden";
+    percentage.style.height = 0;
+  } else {
+    percentage.style.visibility = "visible";
+    percentage.style.height = `${(fullCups / totalCups) * 350}px`;
+    percentage.innerText = `${(fullCups / totalCups) * 100}%`;
+  }
+
+  if (fullCups === totalCups) {
+    remained.style.visibility = "hidden";
+    remained.style.height = 0;
+  } else {
+    remained.style.visibility = "visible";
+    liters.innerText = `${3 - (250 * fullCups) / 1000}L`;
+  }
+}
